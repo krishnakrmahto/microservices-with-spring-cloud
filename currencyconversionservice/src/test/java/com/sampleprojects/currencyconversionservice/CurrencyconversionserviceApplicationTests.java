474e0e9576c85c0e78ecba376b2fc7e9c1f416ef
krishnakrmahto97 <krish.thorcode@gmail.com>
2022-02-17 10:20:40 +0530

    initialized currencyconversionservice spring boot app

---
+++ currencyconversionservice/src/test/java/com/sampleprojects/currencyconversionservice/CurrencyconversionserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampleprojects.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyconversionserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
