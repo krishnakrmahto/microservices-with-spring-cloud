d0a61dc771c4381f6442d0fc46a9301e610c65fe
krishnakrmahto97 <krish.thorcode@gmail.com>
2022-02-20 19:13:41 +0530

    circuit breaker feature of resilience4j

--- currencyexchangeservice/src/main/java/com/sampleprojects/currencyexchangeservice/api/server/rest/CurrencyExchangeController.java
+++ currencyexchangeservice/src/main/java/com/sampleprojects/currencyexchangeservice/api/server/rest/CurrencyExchangeController.java
@@ -3,7 +3,7 @@ package com.sampleprojects.currencyexchangeservice.api.server.rest;
 import com.sampleprojects.currencyexchangeservice.api.server.exception.CurrencyExchangeNotFoundException;
 import com.sampleprojects.currencyexchangeservice.api.server.response.CurrencyExchangeResponse;
 import com.sampleprojects.currencyexchangeservice.service.CurrencyExchangeService;
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +19,8 @@ public class CurrencyExchangeController {
   private final CurrencyExchangeService service;
 
   @GetMapping
-  @Retry(name = "currency-exchange", fallbackMethod = "defaultRetryFallbackEndpoint")
+//  @Retry(name = "currency-exchange", fallbackMethod = "defaultRetryFallbackEndpoint")
+  @CircuitBreaker(name = "currency-exchange", fallbackMethod = "defaultRetryFallbackEndpoint")
   public CurrencyExchangeResponse getFromToCurrencyExchange(@RequestParam String from, @RequestParam String to) {
 
     System.out.println("LocalDateTime.now() " + LocalDateTime.now());
@@ -28,7 +29,8 @@ public class CurrencyExchangeController {
             String.format("Currency exchange for from: %s, to: %s was not found", from, to)));
   }
 
-  private CurrencyExchangeResponse defaultRetryFallbackEndpoint(String from, String to, CurrencyExchangeNotFoundException e) {
+  private CurrencyExchangeResponse defaultRetryFallbackEndpoint(String from, String to, Exception e) {
+    System.out.println("Fallback LOL");
     return CurrencyExchangeResponse.builder()
         .fromCurrency(from)
         .toCurrency(to)
