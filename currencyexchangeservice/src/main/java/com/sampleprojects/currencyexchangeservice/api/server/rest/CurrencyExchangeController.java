a512f457c38bfb425f77a230cd6bdf0deb6425d5
krishnakrmahto97 <krish.thorcode@gmail.com>
2022-02-21 10:27:46 +0530

    added ratelimiter to currency exchange api server

--- currencyexchangeservice/src/main/java/com/sampleprojects/currencyexchangeservice/api/server/rest/CurrencyExchangeController.java
+++ currencyexchangeservice/src/main/java/com/sampleprojects/currencyexchangeservice/api/server/rest/CurrencyExchangeController.java
@@ -3,6 +3,8 @@ package com.sampleprojects.currencyexchangeservice.api.server.rest;
 import com.sampleprojects.currencyexchangeservice.api.server.exception.CurrencyExchangeNotFoundException;
 import com.sampleprojects.currencyexchangeservice.api.server.response.CurrencyExchangeResponse;
 import com.sampleprojects.currencyexchangeservice.service.CurrencyExchangeService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,11 +20,21 @@ public class CurrencyExchangeController {
   private final CurrencyExchangeService service;
 
   @GetMapping
+  @RateLimiter(name = "currency-exchange-get", fallbackMethod = "rateLimiterFallback")
   public CurrencyExchangeResponse getFromToCurrencyExchange(@RequestParam String from, @RequestParam String to) {
 
     System.out.println("LocalDateTime.now() " + LocalDateTime.now());
     return service.getFromToCurrencyExchange(from, to)
         .orElseThrow(() -> new CurrencyExchangeNotFoundException(
             String.format("Currency exchange for from: %s, to: %s was not found", from, to)));
+  }
+
+  private CurrencyExchangeResponse rateLimiterFallback(String from, String to, Exception e) {
+    System.out.println("RateLimiter fallback...");
+//    throw new CurrencyExchangeServerException("The currency exchange server seems to be temporarily unable to process new requests");
+    return CurrencyExchangeResponse.builder()
+        .conversionMultiple(BigDecimal.TEN)
+        .toCurrency("rateLimiterFallback..")
+        .build();
   }
 }
