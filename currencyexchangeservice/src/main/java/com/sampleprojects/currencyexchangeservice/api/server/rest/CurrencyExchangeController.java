d9ede3b76d00e505f50daa22777a57f384226d41
krishnakrmahto97 <krish.thorcode@gmail.com>
2022-02-21 10:01:52 +0530

    removed resilience4j retry and circuit breaker from CurrencyExchangeController because resilience4j (or hystrix for that matter) must be on api client.

--- currencyexchangeservice/src/main/java/com/sampleprojects/currencyexchangeservice/api/server/rest/CurrencyExchangeController.java
+++ currencyexchangeservice/src/main/java/com/sampleprojects/currencyexchangeservice/api/server/rest/CurrencyExchangeController.java
@@ -3,8 +3,6 @@ package com.sampleprojects.currencyexchangeservice.api.server.rest;
 import com.sampleprojects.currencyexchangeservice.api.server.exception.CurrencyExchangeNotFoundException;
 import com.sampleprojects.currencyexchangeservice.api.server.response.CurrencyExchangeResponse;
 import com.sampleprojects.currencyexchangeservice.service.CurrencyExchangeService;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,29 +18,11 @@ public class CurrencyExchangeController {
   private final CurrencyExchangeService service;
 
   @GetMapping
-  @CircuitBreaker(name = "currency-exchange", fallbackMethod = "circuitBreakerFallbackEndpoint")
-  @Retry(name = "currency-exchange", fallbackMethod = "retryFallbackEndpoint")
   public CurrencyExchangeResponse getFromToCurrencyExchange(@RequestParam String from, @RequestParam String to) {
 
     System.out.println("LocalDateTime.now() " + LocalDateTime.now());
     return service.getFromToCurrencyExchange(from, to)
         .orElseThrow(() -> new CurrencyExchangeNotFoundException(
             String.format("Currency exchange for from: %s, to: %s was not found", from, to)));
-  }
-
-  private CurrencyExchangeResponse retryFallbackEndpoint(String from, String to, Exception e) {
-    System.out.println("Retry Fallback");
-    return CurrencyExchangeResponse.builder()
-        .fromCurrency("Dummy Retry currency")
-        .toCurrency(to)
-        .build();
-  }
-
-  private CurrencyExchangeResponse circuitBreakerFallbackEndpoint(String from, String to, Exception e) {
-    System.out.println("CircuitBreaker Fallback");
-    return CurrencyExchangeResponse.builder()
-        .fromCurrency("Dummy CircuitBreaker currency")
-        .toCurrency(to)
-        .build();
   }
 }
